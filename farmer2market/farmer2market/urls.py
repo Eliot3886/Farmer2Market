25155fd27184ba54f33cb9ad3bcb6e8bed4953a3
Eliot <chitowamombeeliot00@gmail.com>
2026-04-29 00:39:27 +0200

    Fix Django settings for Render deployment

--- farmer2market/farmer2market/urls.py
+++ farmer2market/farmer2market/urls.py
@@ -6,7 +6,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Farmer2Market API. The API is running successfully!")
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
